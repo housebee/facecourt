20a39195c9d6b47e78f4d6f338d0a4ab377cff27
Shaopeng Sun <shaopeng.sun@hp.com>
2018-01-05 21:28:38 -0600

    add logging framework

--- src/main/java/com/facecourt/webapp/security/FacebookSignInAdapter.java
+++ src/main/java/com/facecourt/webapp/security/FacebookSignInAdapter.java
@@ -2,6 +2,8 @@ package com.facecourt.webapp.security;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,11 +12,17 @@ import org.springframework.social.connect.web.SignInAdapter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.NativeWebRequest;
 
+import com.facecourt.webapp.controller.HomeController;
+
 @Service
 public class FacebookSignInAdapter implements SignInAdapter {
+
+	// logger
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
 	@Override
 	public String signIn(String localUserId, Connection<?> connection, NativeWebRequest request) {
-		System.out.println(" ====== Sign In adapter, localUserId: " + localUserId);
+		logger.info(" ====== Sign In adapter, localUserId: " + localUserId);
 		SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(
 				connection.getDisplayName(), null, Arrays.asList(new SimpleGrantedAuthority("FACEBOOK_USER"), new SimpleGrantedAuthority("ADMIN"))));
 		return null;
